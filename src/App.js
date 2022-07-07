fa16a8ab30ae640e4fb221139c09d71422bca0d7
Ozi <oguzkaanyildiz@Ozis-MacBook-Air.local>
2022-07-07 15:19:41 +0200

    Making the input search to filter the monsters

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -19,11 +20,32 @@ class App extends Component {
       ))
   }
 
+  onSearchChange = event => {
+    const searchField = event.target.value.toLocaleLowerCase()
+    this.setState(() => {
+      return { searchField }
+    })
+  }
+
   render() {
+
+    const { monsters, searchField } = this.state
+    const { onSearchChange } = this
+
+    const filteredMonsters = monsters.filter(monster => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
+    })
+
     return (
       <div className='App'>
+        <input
+          className='search-box'
+          type='search'
+          placeholeder='search monsters'
+          onChange={onSearchChange}
+        />
         {
-          this.state.monsters.map(monster => {
+          filteredMonsters.map(monster => {
             return (
               <div key={monster.id}>
                 <h1>{monster.name}</h1>
